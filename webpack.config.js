cef5c4cf1b6c203ee6c096e82d8009acd0823848
pestrige <pestrige@gmail.com>
2021-04-01 19:54:01 +0300

    Revert "Исправил gitignore и изменил путь к собранным скриптам"
    This reverts commit dff308590a2a7a0eeeef1ee5b912e21775d704d2.
    Отменил изменения

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public/js'),
+    path: path.resolve(__dirname, 'public'),
   },
   devtool: 'source-map',
   devServer: {
